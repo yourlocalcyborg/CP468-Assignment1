52ba39ceeaa6757e2ff9a0cf7aafe5cb1477a7ca
yourlocalcyborg <duwareon@gmail.com>
2025-06-08 16:00:55 -0400

    Added h3

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
-run the program with ``python a_star.py``
+## Usage
+Run the program with ``python a_star.py``
+
+## Heuristics
+h2 seems to be faster than h1, but h3 (manhattan distances with linear conflicts) seems to be even faster than h2.
+
+They all also seem to be optimal, as they all reach the same conclusion of number of steps.
--- a_star.py
+++ a_star.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import heapq
+import random as r
 
 class State:
     state = None
@@ -17,6 +18,14 @@ class State:
     def copy(self):
         return State(self.state.copy())
 
+    def __str__(self):
+        statestr = ""
+        for i in range(0, 3):
+            for j in range(0, 3):
+                statestr += str(self.state[i*3 + j])
+            statestr += '\n'
+        return statestr
+
     def __eq__(self, other):
         if not isinstance(other, State):
             return NotImplemented
@@ -29,14 +38,6 @@ class State:
     # hash function implemented for heap
     def __hash__(self):
         return hash(tuple(self.state))
-
-    # prints state to terminal
-    def print_state(self):
-        for i in range(0, 3):
-            for j in range(0, 3):
-                print(self.state[i*3 + j], end=" ")
-            print("")
-        print("")
 
     # returns: list of states of possible moves,
     # as in what state nodes are adjacent to the current node in state space 
@@ -78,6 +79,23 @@ class State:
             case 8:
                 return [5, 7]
 
+
+    # number of mismatched tiles with other state
+    # a.k.a. h_1
+    # inputs: current state, target state
+    # returns: number of tiles of difference (not counting empty tile)
+    @staticmethod
+    def num_misplaced_tiles(tiles, targets):
+        tileslist = tiles.state
+        targetslist = targets.state
+
+        total = 0
+        for i in range(0,9):
+            if tileslist[i] != targetslist[i] and tileslist[i] != 0:
+                total += 1
+
+        return total
+
     # sum of horizontal and vertical distances to target,
     # a.k.a. number of moves to move tile to target space
     # inputs: tile index, target index
@@ -95,32 +113,60 @@ class State:
     # returns: sum of all manhattan distances
     @staticmethod
     def sum_manhattan_dists(tiles, targets):
-        tileslist = tiles.state
-        targetslist = targets.state
-
         total = 0
         for i in range(0, 9):
             # ignore calculating dist for empty tile
-            if tileslist[i] != 0:
+            if tiles.state[i] != 0:
                 # get tile number, then find same tilenumber's index in target
-                total += State.calc_manhattan_dist(i, targetslist.index(tileslist[i]))
-
+                total += State.calc_manhattan_dist(i, targets.state.index(tiles.state[i]))
         return total
 
-    # a.k.a. h_1
-    # inputs: current state, target state
-    # returns: number of tiles of difference (not counting empty tile)
+    # Linear conflicts heuristic
+    # a.k.a. h_3
+    # https://youtu.be/8t3lPD2Qtao
+    # https://algorithmsinsight.wordpress.com/graph-theory-2/a-star-in-general/implementing-a-star-to-solve-n-puzzle/ (section on "Linear Conflict + Manhattan Distance/Taxicab geometry")
     @staticmethod
-    def num_misplaced_tiles(tiles, targets):
-        tileslist = tiles.state
-        targetslist = targets.state
+    def linear_conflicts(tiles, targets):
+        def count_conflicts_line(current_state, target_state, line_positions):
+            tiles_in_line = []
+            for pos in line_positions:
+                tile = current_state[pos]
+                if tile != 0:
+                    target_pos = target_state.index(tile)
+                    if target_pos in line_positions:
+                        tiles_in_line.append((tile, pos, target_pos))
+
+            conflicts = 0
+            for i in range(len(tiles_in_line)):
+                for j in range(i + 1, len(tiles_in_line)):
+                    tile1, pos1, target1 = tiles_in_line[i]
+                    tile2, pos2, target2 = tiles_in_line[j]
+                    if (pos1 < pos2) != (target1 < target2):
+                        conflicts += 1
+            return conflicts
 
         total = 0
-        for i in range(0,9):
-            if tileslist[i] != targetslist[i] and tileslist[i] != 0:
-                total += 1
 
-        return total
+        for row in range(3):
+            total += count_conflicts_line(tiles.state, targets.state, [row * 3 + i for i in range(3)])
+
+        for col in range(3):
+            total += count_conflicts_line(tiles.state, targets.state, [i * 3 + col for i in range(3)])
+            
+            
+        return total*2 + State.sum_manhattan_dists(tiles, targets)
+
+    # Counts inversions to determine if the puzzle state is solvable.
+    # For each tile, it counts how many tiles after it are smaller,
+    # then if the number of total inversions are even, it is solvable.
+    def is_solvable(self):
+        puzzle = [t for t in self.state if t != 0]
+        inversions = 0
+        for i in range(len(puzzle)):
+            for j in range(i + 1, len(puzzle)):
+                if puzzle[i] > puzzle[j]:
+                    inversions += 1
+        return inversions % 2 == 0
 
     # A* search
     # Finds lowest f(n) = g(n) + h(n) (where g(n) is the step cost from start, and h(n) is the heuristic cost to goal state),
@@ -192,11 +238,11 @@ goal_list = [0, 1, 2, 3, 4, 5, 6, 7, 8]
 current_state = State(init_list)
 target_state = State(goal_list)
 print("test printing")
-current_state.print_state()
+print(current_state)
 
 print("test neighbours")
 for i in current_state.get_neighbours():
-    i.print_state()
+    print(i)
 
 print("test manhattan distance")
 print("dist from (0,0) to (0,1):", State.calc_manhattan_dist(0, 3), "(1)")
@@ -207,16 +253,16 @@ print("")
 
 print("test sum manhattan distances\n")
 print("sum of\n")
-current_state.print_state()
+print(current_state)
 print("distances to\n")
-target_state.print_state()
+print(target_state)
 print("equals", State.sum_manhattan_dists(current_state, target_state), "\n")
 
 print("test misplaced tiles")
 print("using init and target as previous,", State.num_misplaced_tiles(current_state, target_state), "(8)\n")
 print("using")
 new_state = State([0,1,3,2,5,6,8,7,4])
-new_state.print_state()
+print(new_state)
 print("and target as previous,", State.num_misplaced_tiles(new_state, target_state), "(6)")
 print("")
 
@@ -230,13 +276,40 @@ print("test A*")
 print("solution with h_1:")
 soln, soln_steps, nodes_expanded = current_state.a_star(target_state, State.num_misplaced_tiles)
 for i in soln:
-    i.print_state()
+    print(i)
 print("steps:", soln_steps)
 print("nodes_expanded:", nodes_expanded)
+print("")
 
 print("solution with h_2:")
 soln, soln_steps, nodes_expanded = current_state.a_star(target_state, State.sum_manhattan_dists)
 for i in soln:
-    i.print_state()
+    print(i)
 print("steps:", soln_steps)
 print("nodes_expanded:", nodes_expanded)
+print("")
+
+print("solution with h_3:")
+soln, soln_steps, nodes_expanded = current_state.a_star(target_state, State.linear_conflicts)
+for i in soln:
+    print(i)
+print("steps:", soln_steps)
+print("nodes_expanded:", nodes_expanded)
+print("")
+
+print("Generate 100 random puzzles to solve")
+num_generations = 100
+states = []
+print("| #   | State                       | h1 steps | h1 exp. nodes | h2 steps | h2 exp. nodes | h3 steps | h3 exp. nodes |")
+print("|-----|-----------------------------|----------|---------------|----------|---------------|----------|---------------|")
+for i in range(num_generations):
+    statelist = [0,1,2,3,4,5,6,7,8]
+    state = State(statelist)
+    r.shuffle(state.state)
+    while (not state.is_solvable()) or (state in states):
+        r.shuffle(state.state)        
+    states.append(state)
+    h1_results = state.a_star(State([0,1,2,3,4,5,6,7,8]), State.num_misplaced_tiles)
+    h2_results = state.a_star(State([0,1,2,3,4,5,6,7,8]), State.sum_manhattan_dists)
+    h3_results = state.a_star(State([0,1,2,3,4,5,6,7,8]), State.linear_conflicts)
+    print(f"| {i+1:3} | {state.state} | {h1_results[1]:8} | {h1_results[2]:13} | {h1_results[1]:8} | {h2_results[2]:13} | {h3_results[1]:8} | {h3_results[2]:13} |")
